0128a6624bda245366e85a6b0011ffc1e0d7dbaa
WolMoFGHTR <elmof2010@gmail.com>
2026-07-12 14:46:09 +0100

    A Version with kaiming init and BatchNorm

---
+++ MLP Model/Optimization with BatchNorm/BatchNorm Optimization.py
@@ -0,0 +1,139 @@
+import torch
+import matplotlib.pyplot as plt
+# here we generally make our dataset ready !
+
+with open("01.txt","r") as o:
+    data = o.read()
+data = data.splitlines()
+characters = sorted(list(set("".join(data))))
+stoi = {s:i+1 for i,s in enumerate(characters)}
+stoi["."] = 0
+itos = {i:s for s,i in stoi.items()}
+
+# hyperparameters
+Neuron = 20
+embedding_size = 2
+character_size = 54
+itterations = 28000
+learning_rate = 0.1
+
+# clearing our dataset so we can make it useful by setting the inputs and labels
+blocksize = 3
+def dataset_Builder(words):
+    
+    X , Y = [], []
+    for w in words:
+        #print(w)
+        context = [0] * blocksize
+        for ch in w+".":
+            ix = stoi[ch]
+            X.append(context)
+            Y.append(ix)
+            # print("".join(itos[o] for o in context), "=>" , itos[ix])
+            context = context[1:] + [ix]
+            # print(context)
+    X = torch.tensor(X)
+    Y = torch.tensor(Y)
+    return X , Y
+import random
+random.seed(42)
+random.shuffle(data)
+n1 = int(0.8*len(data))
+n2 = int(0.9*len(data))
+xtr , ytr = dataset_Builder(data[:n1])
+xdev , ydev = dataset_Builder(data[n1:n2])
+xte , yte = dataset_Builder(data[n2:])
+
+# Parameters Getting Ready !
+
+o = torch.Generator().manual_seed(4654675)
+C = torch.randn((character_size,embedding_size),generator=o)
+W1 = torch.randn((embedding_size * blocksize, Neuron),generator=o) * (5/3) / ((embedding_size * blocksize) ** 0.5) # Xavier Initialization
+B1 = torch.randn(Neuron,generator=o) * 0.1 
+W2 = torch.randn((Neuron,character_size),generator=o) * 0.1     #XAVIER later
+B2 = torch.randn(character_size,generator=o) * 0  
+bnweights = torch.ones((1,Neuron))
+bnbias = torch.zeros((1,Neuron))
+mean = torch.zeros((1,Neuron))
+std = torch.ones((1,Neuron))
+
+
+params = [W1,W2,B1,B2,C,bnweights,bnbias]
+
+HowBIgIsthenueronnet = sum(h.nelement() for h in params)
+print("Number of parameters in the model is: ", HowBIgIsthenueronnet)
+for i in params:
+    i.requires_grad=True
+KOs = torch.linspace(-3,0,1000)
+Sos = 10 ** KOs 
+lossi = []
+learni = []
+for i in range(28000):
+    ixs = torch.randint(0, xtr.shape[0], (32,),generator=o) #MINIBATCH 
+    # Forward Pass
+    embeddings = C[xtr[ixs]]
+    # Linear Layer
+    h = embeddings.view(embeddings.shape[0] , blocksize * embedding_size) @ W1 # + B1 Got Cancelled On The BatchNorm Layer 
+    # Batch Normalization Layer
+    meani = h.mean(dim=0,keepdim=True)
+    stdi = h.std(dim=0,keepdim=True)
+    h = bnweights * ((h - meani) / stdi) + bnbias
+    with torch.no_grad():
+        mean = 0.999 * mean + 0.001 * meani
+        std = 0.999 * std + 0.001 * stdi
+    # Non Linearity Layer
+    values = torch.tanh(h)
+    # Loss Calculation For BackProp Later And Learning ! 
+    logits = values @ W2 + B2
+    # counts = logits.exp()
+    # prob = (counts / counts.sum(1, keepdim = True))
+    # softmaxLOSS = -prob[torch.arange(5072),Y].log().mean() # generally speaking i find out that there is that cross_entropy function which does all this softmax stuff and more fficient well behaved numerically and tada
+    softmaxLOSS = torch.nn.functional.cross_entropy(logits , ytr[ixs]) 
+
+    # Backward Pass
+    
+    for s in params:
+        s.grad = None
+    softmaxLOSS.backward()
+
+    # lossi.append(softmaxLOSS.item())
+    # learni.append(Sos[i]) plot things to see learni on function of lossi
+    
+    # Update !:
+    lr = learning_rate  
+    for Relaxing in params:
+        Relaxing.data += - lr * Relaxing.grad #Sos[i]
+# print("good Job AI Your LOSS is:" , softmaxLOSS.item())
+# plt.plot(learni,lossi)
+# plt.show() to plot for sure
+    # Forward Pass
+embeddings = C[xdev]
+h = embeddings.view(embeddings.shape[0] , 6) @ W1 + B1
+h = bnweights * ((h - mean) / std) + bnbias # we used the mean and std from the training set to normalize the dev set as we do with weights and bias from the 80% training set to the 10% dev set
+values = torch.tanh(h)
+logits = values @ W2 + B2
+    # counts = logits.exp()
+    # prob = (counts / counts.sum(1, keepdim = True))
+    # softmaxLOSS = -prob[torch.len(Y.item),Y].log().mean() # generally speaking i find out that there is that cross_entropy function which does all this softmax stuff and more fficient well behaved numerically and tada
+softmaxLOSS = torch.nn.functional.cross_entropy(logits , ydev)
+#print("good Job AI Your LOSS is:" , softmaxLOSS.item())
+
+
+# Sampling From The Trained Neural Net
+gen = torch.Generator().manual_seed(15151515 + 10)
+
+for _ in range(20):
+    Context = [0] * blocksize
+    out = []
+    while True:
+        emb = C[torch.tensor(Context)]
+        h = bnweights * ((emb.view(1 , 6) @ W1 + B1 - mean) / std) + bnbias
+        fst = torch.tanh(h)
+        logits = fst @ W2 + B2
+        probs = torch.softmax(logits,dim=1)
+        ix = torch.multinomial(probs , num_samples=1 , generator=gen)
+        Context = Context[1:] + [ix.item()]
+        out.append(itos[ix.item()])
+        if ix.item() == 0:
+            break
+    print("".join(out))
