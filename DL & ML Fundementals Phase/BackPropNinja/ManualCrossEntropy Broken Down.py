e94de1f79994be5c8a2ec1e8b11b8fc9fb0517f2
WOOLMO <elmof2010@gmail.com>
2026-07-23 02:06:17 +0100

    Update

--- DL & ML Fundementals Phase/BackPropNinja/ManualCrossEntropy Broken Down.py
+++ DL & ML Fundementals Phase/BackPropNinja/ManualCrossEntropy Broken Down.py
@@ -1,2 +1,7 @@
 import torch 
 logits = torch.randn(3, 6)
+# we go our logits after the non linearity function , so we need to apply the softmax function to get the probabilities! and the loss function generally.
+max_logits = logits.max(dim=1,keepdim=True).values
+stable_logits = logits - max_logits
+
+exponentials = torch.exp(stable_logits)
