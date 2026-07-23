5b5782f418a4b6565c1471c883e4b82ca43082cc
WOOLMO <elmof2010@gmail.com>
2026-07-23 02:31:58 +0100

    Update

--- DL & ML Fundementals Phase/BackPropNinja/ManualCrossEntropy Broken Down.py
+++ DL & ML Fundementals Phase/BackPropNinja/ManualCrossEntropy Broken Down.py
@@ -1,4 +1,5 @@
 import torch 
+labels = torch.tensor([1 , 0 , 5])
 logits = torch.randn(3, 6)
 # we go our logits after the non linearity function , so we need to apply the softmax function to get the probabilities! and the loss function generally.
 max_logits = logits.max(dim=1,keepdim=True).values
@@ -6,3 +7,10 @@ stable_logits = logits - max_logits
 
 exponentials = torch.exp(stable_logits)
 sum_exponentials = exponentials.sum(dim=1, keepdim=True)
+inv_sum_exponentials = sum_exponentials ** -1
+probabilities = exponentials * inv_sum_exponentials
+# Softmax function is now applied and we are ready to calculate the loss function. 
+logprobabilities = torch.log(probabilities)
+loss = -logprobabilities[range(len(labels)), labels ].mean()
+# Now As You See Those Two Lines Are The Implementation Of The Cross Entropy Loss Function.
+print(loss) # Just Curious To See The Loss Value.  
